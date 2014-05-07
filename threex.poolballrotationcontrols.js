d7d1552d55c146e8d2296d86c45963d123e3587f
Jerome Etienne <jerome.etienne@gmail.com>
2014-05-07 15:25:26 +0200

    more flexible better demo.html

---
+++ threex.poolballrotationcontrols.js
@@ -0,0 +1,48 @@
+var THREEx	= THREEx	|| {}
+
+/**
+ * Creator for tQuery.PoolBall
+ * @return {tQuery.Mesh} instance of 
+ */
+THREEx.PoolBallRotationControls	= function(object3d){
+
+	this.object3d	= object3d
+	object3d.matrixAutoUpdate = false;
+	object3d.updateMatrix();
+
+	var prevPosition	= new THREE.Vector3()
+	prevPosition.copy(object3d.position)
+
+	this.update	= function(delta){
+		// compute velocity
+		var position	= object3d.position
+		var velocity	= position.clone().sub(prevPosition)
+		prevPosition.copy(position)
+
+		// update matrix
+		object3d.updateMatrix();
+
+		// solution to get rolling marble from |3d| on freenode
+		var translation	= new THREE.Vector3(position.x,position.y,position.z)
+		var matrix	= new THREE.Matrix4().makeTranslation(translation.x, translation.y, translation.z);
+		// TODO make it radius independant
+		// c = circumference
+		// (velocity/(2*PI*radius)) * Math.PI*2
+		var radius	= object3d.geometry.radius * object3d.scale.x
+		// matrix.multiply(  new THREE.Matrix4().makeRotationZ(-velocity.x*(Math.PI/2*radius)));
+		// matrix.multiply(  new THREE.Matrix4().makeRotationX( velocity.z*(Math.PI/2*radius)));
+		matrix.multiply(  new THREE.Matrix4().makeRotationZ(-velocity.x*(Math.PI/2*radius)));
+		matrix.multiply(  new THREE.Matrix4().makeRotationX( velocity.z*(Math.PI/2*radius)));
+		matrix.multiply(
+			new THREE.Matrix4().makeTranslation(-translation.x, -translation.y, -translation.z)
+		);
+		matrix.multiply(object3d.matrix)
+		object3d.matrix.copy(matrix);
+
+		// put back the rotation
+		object3d.rotation.setFromRotationMatrix( object3d.matrix, object3d.rotation.order );
+
+		// update matrixWorld
+		object3d.updateMatrixWorld(true);		
+	}
+}
